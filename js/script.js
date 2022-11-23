64b0f1b9b7b244bf8594378b6972213c161712e5
GiorgioDellutri <giorgiodll93@gmail.com>
2022-11-23 17:26:06 +0100

    if divisible by 3 and 5

--- js/script.js
+++ js/script.js
@@ -1,38 +1,25 @@
-// Consegna:
-// Scrivi un programma che stampi in console i numeri da 1 a 100:
-// ma che per i multipli di 3 stampi “Fizz” al posto del numero e
-// per i multipli di 5 stampi “Buzz”.
-// Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.
-// Prima di partire a scrivere codice poniamoci qualche domanda:
-// Come faccio a sapere se un numero è divisibile per un altro? Abbiamo visto qualcosa di particolare che possiamo usare?
-// Consigli del giorno:
-// Scriviamo sempre prima dei commenti in italiano per capire cosa vogliamo fare
-// Proviamo ad immaginare le operazioni che vogliamo far svolgere al nostro programma così come lo faremmo "a mano"
-// Note: i bonus sono da considerarsi validi solo E SOLTANTO se avete finito l'esercizio base con successo.
-// BONUS 1:
-// Crea un container nel DOM , aggiungendo (attraverso la funzione append()) un elemento html con il numero o la stringa corretta da mostrare.
-// BONUS 2:
-// Applica stili differenti agli elementi aggiunti al DOM nel BONUS 1, a seconda che il valore inserito sia un numero, un fizz, un buzz o un fizzbuzz.
-// :party_blob: :party_blob: :party_blob: Buon lavoro e buon divertimento! :arrossire:  :party_blob: :party_blob: :party_blob:
-// (modificato)
-
-
-
 for (let i = 1; i <= 100; i++) {
-    console.log(i);
+    if (((i %  3) == 0 ) && ((i % 5) == 0)){
+
+    console.log('fizbuzz')
+    }
 
     if ( (i %  3) == 0 ) {
 
-        let i = 'fizz';
-        console.log(i);
+        console.log('fizz');
     } else if ((i % 5) == 0){
 
-        let i = 'buzz';
-        console.log(i);
-    } 
-        // else (((i %  3) & (i % 5) )== 0) {
-
-        //     let i = 'fizzbuzz';
-        //     console.log(i)
-        // }
+        console.log('buzz');
+    } else {
+        console.log(i)
+    }
 }
+
+
+
+// BONUS 1:
+// Crea un container nel DOM , aggiungendo (attraverso la funzione append()) un elemento html con il numero o la stringa corretta da mostrare.
+
+// BONUS 2:
+// Applica stili differenti agli elementi aggiunti al DOM nel BONUS 1, a seconda che il valore inserito sia un numero, un fizz, un buzz o un fizzbuzz.
+
