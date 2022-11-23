0697e4d7ea41b1cee2cfb0441389902868495b9d
GiorgioDellutri <giorgiodll93@gmail.com>
2022-11-23 15:27:38 +0100

    index

---
+++ js/script.js
@@ -0,0 +1,38 @@
+// Consegna:
+// Scrivi un programma che stampi in console i numeri da 1 a 100:
+// ma che per i multipli di 3 stampi “Fizz” al posto del numero e
+// per i multipli di 5 stampi “Buzz”.
+// Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.
+// Prima di partire a scrivere codice poniamoci qualche domanda:
+// Come faccio a sapere se un numero è divisibile per un altro? Abbiamo visto qualcosa di particolare che possiamo usare?
+// Consigli del giorno:
+// Scriviamo sempre prima dei commenti in italiano per capire cosa vogliamo fare
+// Proviamo ad immaginare le operazioni che vogliamo far svolgere al nostro programma così come lo faremmo "a mano"
+// Note: i bonus sono da considerarsi validi solo E SOLTANTO se avete finito l'esercizio base con successo.
+// BONUS 1:
+// Crea un container nel DOM , aggiungendo (attraverso la funzione append()) un elemento html con il numero o la stringa corretta da mostrare.
+// BONUS 2:
+// Applica stili differenti agli elementi aggiunti al DOM nel BONUS 1, a seconda che il valore inserito sia un numero, un fizz, un buzz o un fizzbuzz.
+// :party_blob: :party_blob: :party_blob: Buon lavoro e buon divertimento! :arrossire:  :party_blob: :party_blob: :party_blob:
+// (modificato)
+
+const fizz = 'fizz';
+const buzz = 'buzz';
+const fizzbuzz = 'fizzbuzz'
+
+for (let i = 1; i <= 100; i++) {
+    console.log(i);
+    // if (i++ % 3){
+        
+    // } 
+    
+    // if (index++ % 5){
+        
+    //     console.log(buzz);
+    // }
+
+    // if ((index++ % 3) & (index++ % 5)){
+
+    //     console.log(fizzbuzz)
+    // }
+}
